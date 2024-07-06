833b2b1ac240067041fa3ff46769119b169386a7
anshu1016 <arunshukla98710@gmail.com>
2024-07-06 14:39:21 +0530

    Database done

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser"
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import { error } from "console";
+import KPI from "./models/KPI";
+import { kpis } from "./data/data";
+
+/**CONFIGURATIONS */
+dotenv.config();
+const app = express()
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+console.log("hellooooooooooo")
+const PORT = process.env.PORT || 9000;
+
+app.use("/kpi",kpiRoutes)
+
+
+
+mongoose
+.connect(process.env.MONGO_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
+.then(async()=>{
+    app.listen(PORT, ()=>console.log(`PORT IS LISTENING AT ${PORT}`))
+    await mongoose.connect.db.dropDataBase()
+    KPI.insertMany(kpis);
+})
+.catch((error)=> console.log(`DID NOT CONNECT: ${error}`))
